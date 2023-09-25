102ee0942061d8fc69ee2f564604626fd15d3da5
chisacam <dladsds123@gmail.com>
2023-09-25 10:11:17 +0900

    chore: apply gofmt

--- internal/models/document.go
+++ internal/models/document.go
@@ -6,8 +6,8 @@ import (
 
 type Document struct {
 	gorm.Model
-	UserId int64 `from:"user_id" json:"user_id"`
-	WorkflowId int64 `from:"workflow_id" json:"workflow_id"`
-	Title string `from:"title" json:"title"`
-	Content string `from:"content" json:"content"`
+	UserId     int64  `from:"user_id" json:"user_id"`
+	WorkflowId int64  `from:"workflow_id" json:"workflow_id"`
+	Title      string `from:"title" json:"title"`
+	Content    string `from:"content" json:"content"`
 }
